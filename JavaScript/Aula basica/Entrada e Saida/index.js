b9610e2c55585507d5f252eb1ac7df78e1122492
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2021-11-23 18:53:56 -0300

    javascript

---
+++ JavaScript/Aula basica/Entrada e Saida/index.js
@@ -0,0 +1,8 @@
+var nome = prompt("Digite o seu nome: ");
+var idade = prompt("Digite a sua idade: ");
+
+console.log("Seu nome é " + nome +"sua idade é " + idade)// aqui ele esta apresentando a resposta no console do navegador.
+
+//exemplo abaixo mostrando o resultado diretamente no HTML como comando document.write.
+document.write("Seu nome é " + nome)
+document.write("Sua idade é " + idade)
--- JavaScript/Aula basica/aula 1/script.js
+++ JavaScript/Aula basica/aula 1/script.js
@@ -1,1 +1,2 @@
 var nome = "junior";
+alert(nome)
